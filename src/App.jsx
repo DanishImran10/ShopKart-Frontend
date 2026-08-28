eca1a2338be869fca2241f087316b90ba31928b5
Danish Imran <danishimran015@gmail.com>
2026-08-29 01:24:45 +0500

    all pages converted to react, routes established

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
+import { Routes, Route } from 'react-router'
 import './App.css'
 import HomePage from './Components/HomePage'
+import OrdersPage from './Components/OrdersPage'
+import TrackingPage from './Components/TrackingPage'
+import CheckoutPage from './Components/CheckoutPage'
 
 function App() {
-  return <HomePage />
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/orders" element={<OrdersPage />} />
+      <Route path="/tracking" element={<TrackingPage />} />
+      <Route path="/checkout" element={<CheckoutPage />} />
+    </Routes>
+  )
 }
 
 export default App
